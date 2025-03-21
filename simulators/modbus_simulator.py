9db378aa3c375ef70b4b5eb2467448efa9c1e7b1
Acobf94 <acoran.bonilla94@gmail.com>
2025-03-21 11:14:34 +0000

    Update modbus_simulator.py

--- simulators/modbus_simulator.py
+++ simulators/modbus_simulator.py
@@ -7,10 +7,10 @@ from pymodbus.server.sync import StartTcpServer
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSlaveContext, ModbusServerContext
 
-os.makedirs('/home/abonilla/honeypot/logs', exist_ok=True)
+os.makedirs('/home/nombredeusuario/honeypot/logs', exist_ok=True)
 
 logging.basicConfig(
-    filename='/home/abonilla/honeypot/logs/honeypot.log',
+    filename='/home/nombredeusuario/honeypot/logs/honeypot.log',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
