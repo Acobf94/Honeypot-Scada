3eac55aee0f3d7f6556d0b40162989730d37cffc
Acobf94 <acoran.bonilla94@gmail.com>
2025-03-21 11:13:03 +0000

    Update opcua_simulator.py

--- simulators/opcua_simulator.py
+++ simulators/opcua_simulator.py
@@ -4,10 +4,10 @@ import logging
 import os
 from opcua import Server
 
-os.makedirs('/home/abonilla/honeypot/logs', exist_ok=True)
+os.makedirs('/home/nombredeusuario/honeypot/logs', exist_ok=True)
 
 logging.basicConfig(
-    filename='/home/abonilla/honeypot/logs/honeypot.log',
+    filename='/home/nombredeusuario/honeypot/logs/honeypot.log',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
